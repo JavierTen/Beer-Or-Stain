260f564924104b6904df1557a8c8fc2afc8384a4
juancamiloupb <64169269+juancamiloupb@users.noreply.github.com>
2020-10-12 19:07:03 -0500

    Text Box

---
+++ Assets/Scripts/TextComand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextComand : MonoBehaviour
+{
+    public string content_text = "Alguien tiene la carta!!";
+    public Text texto_cuadro;
+    public GameObject cuadro;
+    public GameObject btn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cuadro.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Showtext()
+    {
+        texto_cuadro.text = content_text;
+        cuadro.SetActive(true);
+        btn.SetActive(true);
+        
+    }
+    public void CerrarText()
+    {
+        cuadro.SetActive(false);
+        btn.SetActive(true);
+    }
+
+    
+}
