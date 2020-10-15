5c44c93a0483625b844f1f3162ebbbb822421b5a
Jaten <33936505+JavierTen@users.noreply.github.com>
2020-10-14 21:18:05 -0500

    Show Player in match

--- Assets/Scripts/ShowIdMatch.cs
+++ Assets/Scripts/ShowIdMatch.cs
@@ -13,10 +13,60 @@ public class ShowIdMatch : MonoBehaviour
     private MySqlCommand consola;
 
     public Text idPartida;
+    public Text jugadores;
 
     void Start()
     {
         MostrarIdPartida();
+    }
+
+    public float timeRemaining = 1;
+    private bool gamerunning;
+
+    //static var dPartida;
+
+    void Update()
+    {
+        if (!gamerunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 10;
+                try
+                {
+                    Debug.Log(idPartida);
+                    string DataConecction = "Server=beerorstain20.mysql.database.azure.com; Port=3306; Database=bosdb; Uid=adminbos@beerorstain20; Pwd=*camaja20*; SslMode=Preferred;";
+                    string Query = "SELECT nombre FROM bosdb.jugador where idPartida = 4018;";
+
+                    conexion = new MySqlConnection(DataConecction);
+                    consola = new MySqlCommand(Query, conexion);
+
+                    conexion.Open();
+                    object result = consola.ExecuteScalar();
+                    if (result != null)
+                    {
+
+
+                        jugadores.text = result+"\n";
+                        Debug.Log(jugadores);
+                    }
+                    conexion.Close();
+
+                }
+                catch (MySqlException ex)
+                {
+
+                    Debug.LogError("Error: " + ex);
+                }
+            }
+        }
+
+
+
     }
 
     void MostrarIdPartida()
