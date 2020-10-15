597413ca131dba39b05ab643e46ed8f6ba8ef77a
juancamiloupb <64169269+juancamiloupb@users.noreply.github.com>
2020-10-15 11:06:40 -0500

    Update showidmatch

--- Assets/Scripts/ShowIdMatch.cs
+++ Assets/Scripts/ShowIdMatch.cs
@@ -16,56 +16,108 @@ public class ShowIdMatch : MonoBehaviour
     public Text idPartida;
     public Text jugadores;
 
-    ArrayList cards = new ArrayList();
+
+    ArrayList cardspiramide = new ArrayList();
+    ArrayList cardsplayers = new ArrayList();
 
 
     void Start()
     {
         MostrarIdPartida();
-        
-        cards.Add("B0");
-        cards.Add("B1");
-        cards.Add("B2");
-        cards.Add("B3");
-        cards.Add("B4");
-        cards.Add("B5");
-        cards.Add("B6");
-        cards.Add("B7");
-        cards.Add("B8");
-        cards.Add("B9");
-        cards.Add("Y0");
-        cards.Add("Y1");
-        cards.Add("Y2");
-        cards.Add("Y3");
-        cards.Add("Y4");
-        cards.Add("Y5");
-        cards.Add("Y6");
-        cards.Add("Y7");
-        cards.Add("Y8");
-        cards.Add("Y9");
-        cards.Add("G0");
-        cards.Add("G1");
-        cards.Add("G2");
-        cards.Add("G3");
-        cards.Add("G4");
-        cards.Add("G5");
-        cards.Add("G6");
-        cards.Add("G7");
-        cards.Add("G8");
-        cards.Add("G9");
-        cards.Add("R0");
-        cards.Add("R1");
-        cards.Add("R2");
-        cards.Add("R3");
-        cards.Add("R4");
-        cards.Add("R5");
-        cards.Add("R6");
-        cards.Add("R7");
-        cards.Add("R8");
-        cards.Add("R9");
+
+        cardspiramide.Add("B0");
+        cardspiramide.Add("B1");
+        cardspiramide.Add("B2");
+        cardspiramide.Add("B3");
+        cardspiramide.Add("B4");
+        cardspiramide.Add("B5");
+        cardspiramide.Add("B6");
+        cardspiramide.Add("B7");
+        cardspiramide.Add("B8");
+        cardspiramide.Add("B9");
+        cardspiramide.Add("Y0");
+        cardspiramide.Add("Y1");
+        cardspiramide.Add("Y2");
+        cardspiramide.Add("Y3");
+        cardspiramide.Add("Y4");
+        cardspiramide.Add("Y5");
+        cardspiramide.Add("Y6");
+        cardspiramide.Add("Y7");
+        cardspiramide.Add("Y8");
+        cardspiramide.Add("Y9");
+        cardspiramide.Add("G0");
+        cardspiramide.Add("G1");
+        cardspiramide.Add("G2");
+        cardspiramide.Add("G3");
+        cardspiramide.Add("G4");
+        cardspiramide.Add("G5");
+        cardspiramide.Add("G6");
+        cardspiramide.Add("G7");
+        cardspiramide.Add("G8");
+        cardspiramide.Add("G9");
+        cardspiramide.Add("R0");
+        cardspiramide.Add("R1");
+        cardspiramide.Add("R2");
+        cardspiramide.Add("R3");
+        cardspiramide.Add("R4");
+        cardspiramide.Add("R5");
+        cardspiramide.Add("R6");
+        cardspiramide.Add("R7");
+        cardspiramide.Add("R8");
+        cardspiramide.Add("R9");
+
+        cardsplayers.Add("B0");
+        cardsplayers.Add("B1");
+        cardsplayers.Add("B2");
+        cardsplayers.Add("B3");
+        cardsplayers.Add("B4");
+        cardsplayers.Add("B5");
+        cardsplayers.Add("B6");
+        cardsplayers.Add("B7");
+        cardsplayers.Add("B8");
+        cardsplayers.Add("B9");
+        cardsplayers.Add("Y0");
+        cardsplayers.Add("Y1");
+        cardsplayers.Add("Y2");
+        cardsplayers.Add("Y3");
+        cardsplayers.Add("Y4");
+        cardsplayers.Add("Y5");
+        cardsplayers.Add("Y6");
+        cardsplayers.Add("Y7");
+        cardsplayers.Add("Y8");
+        cardsplayers.Add("Y9");
+        cardsplayers.Add("G0");
+        cardsplayers.Add("G1");
+        cardsplayers.Add("G2");
+        cardsplayers.Add("G3");
+        cardsplayers.Add("G4");
+        cardsplayers.Add("G5");
+        cardsplayers.Add("G6");
+        cardsplayers.Add("G7");
+        cardsplayers.Add("G8");
+        cardsplayers.Add("G9");
+        cardsplayers.Add("R0");
+        cardsplayers.Add("R1");
+        cardsplayers.Add("R2");
+        cardsplayers.Add("R3");
+        cardsplayers.Add("R4");
+        cardsplayers.Add("R5");
+        cardsplayers.Add("R6");
+        cardsplayers.Add("R7");
+        cardsplayers.Add("R8");
+        cardsplayers.Add("R9");
+
+        cardsplayers.Add("RegA");
+        cardsplayers.Add("RegC");
+        cardsplayers.Add("RegJ");
+        cardsplayers.Add("RegM");
+        cardsplayers.Add("RegP");
+        cardsplayers.Add("RegU");
+        cardsplayers.Add("RegK");
+        cardsplayers.Add("RegN");
+        cardsplayers.Add("RegR");
+        cardsplayers.Add("RegV");
     }
-
-
 
     public float timeRemaining = 1;
     private bool gamerunning;
