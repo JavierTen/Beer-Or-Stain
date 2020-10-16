a06dae3d2e31234225714469a6c68ecad5c3515a
Jaten <33936505+JavierTen@users.noreply.github.com>
2020-10-16 10:26:08 -0500

    Revert "Remake BOS 2"
    This reverts commit 162654cd5efeb379c4d6d8f20cba3876a3e5b309.

--- Assets/Scripts/NewCardShow.cs
+++ Assets/Scripts/NewCardShow.cs
@@ -1,91 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MySql.Data.MySqlClient;
-using System.Data;
-using UnityEngine.UI;
-
-
 
 public class NewCardShow : MonoBehaviour
 {
-
-    private SpriteRenderer rend;
-    private Sprite cardNew;
+    
+    public Sprite cardNew;
     // Update is called once per frame
+    float x, y, z;
     public float timeRemaining = 10;
     private bool gamerunning;
 
-    string idJugador = "";
-    int i = 0;
-    private MySqlConnection conexion;
-    private MySqlCommand consola;
-
-    ArrayList cardspiramide = new ArrayList();
-
-    /* void Awake()
-     {
-         try
-         {
-
-             string DataConecction = "Server=beerorstain20.mysql.database.azure.com; Port=3306; Database=bosdb; Uid=adminbos@beerorstain20; Pwd=*camaja20*; SslMode=Preferred;";
-             string Query = "SELECT idJugador FROM bosdb.jugador WHERE idPartida = " + DatoUsuarios.idPartida + " AND nombre = '" + DatoUsuarios.Nick + "';";
-
-             conexion = new MySqlConnection(DataConecction);
-             consola = new MySqlCommand(Query, conexion);
-
-
-             conexion.Open();
-             MySqlDataReader reader = consola.ExecuteReader();
-             while (reader.Read())
-             {
-                 idJugador = reader["idJugador"].ToString();
-
-             }
-
-             Debug.Log(idJugador);
-             conexion.Close();
-
-         }
-         catch (MySqlException ex)
-         {
-             Debug.LogError("Error: " + ex);
-         }
-     }*/
-    void Start()
-    {
-        try
-        {
-
-            string DataConecction = "Server=beerorstain20.mysql.database.azure.com; Port=3306; Database=bosdb; Uid=adminbos@beerorstain20; Pwd=*camaja20*; SslMode=Preferred;";
-            string Query = "SELECT nombre FROM bosdb.cartajugador WHERE idPartida = 4044 AND idJugador = 73;";
-
-            conexion = new MySqlConnection(DataConecction);
-            consola = new MySqlCommand(Query, conexion);
-
-            string carta = "";
-            conexion.Open();
-            MySqlDataReader reader = consola.ExecuteReader();
-            while (reader.Read())
-            {
-                carta = reader["nombre"].ToString();
-                cardspiramide.Add(carta);
-            }
-
-
-
-            conexion.Close();
-
-        }
-        catch (MySqlException ex)
-        {
-            Debug.LogError("Error: " + ex);
-        }
-    }
+    
 
     void Update()
     {
-
         if (!gamerunning)
         {
             if (timeRemaining > 0)
@@ -94,29 +23,46 @@ public class NewCardShow : MonoBehaviour
             }
             else
             {
+<<<<<<< Updated upstream
+                timeRemaining = 10;
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = cardNew;
+                transform.localScale = new Vector3(0.2156625f, 0.2178207f, 1f);
+=======
                 timeRemaining = 20;
                 if (i < 5)
                 {
                     DatosGlobales.CartaMostrada = cardspiramide[i].ToString();
                     rend = GetComponent<SpriteRenderer>();
-                    cardNew = Resources.Load<Sprite>("Images/" + cardspiramide[i] + "");
+                    cardNew = Resources.Load<Sprite>("Images/" + cardspiramide[i]);
                     rend.sprite = cardNew;
                     i = i+1;
                     transform.localScale = new Vector3(0.2156625f, 0.2178207f, 1f);
                 }
 
+>>>>>>> Stashed changes
             }
         }
+        
+        
 
     }
+<<<<<<< Updated upstream
+=======
 
-    void CartaMostrar()
-    {
 
-    }
-
+>>>>>>> Stashed changes
     public void ChangeGameRunningState()
     {
+        
         gamerunning = !gamerunning;
+
+        if (gamerunning)
+        {
+            Debug.Log("Game corriendo");
+        }
+        else
+        {
+            Debug.Log("game paused");
+        }
     }
 }
